5dec7ad516c7427a1d8fc304960e94d0270d5c17
jspast <140563347+jspast@users.noreply.github.com>
2024-01-14 23:09:58 -0300

    Teste básico insere/consulta

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "lse.h"
 #include "rn.h"
+#include "senha.h"
 
 int main(int argc, char *argv[])
 {
-	
+    PtNo *l;
+    RNtree *t = NodoNULL;
+
+    // Teste LSE
+    l = lse_cria_lista();
+	l = lse_insere(l, (InfoNo){1, "teste"});
+    if(strcmp(lse_consulta(l, 1), "teste") == 0)
+        puts("Teste LSE: Senha correta!");
+
+    // Teste RN
+    t = rn_insere(t, (InfoNo){1, "teste"});
+    if(strcmp(rn_consulta(t, 1), "teste") == 0)
+        puts("Teste RN: Senha correta!");
+
 	return 0;
 }
