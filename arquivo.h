4c254ab49d03b136b5e7a7afaf9c282e456c6997
leal0607 <gleal0607@gmail.com>
2024-01-16 18:57:24 -0300

    Trab alpha

---
+++ arquivo.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+FILE *arq1; //arquivo com os logins
+arq1 = fopen("arq1.txt","r"); // abertura para leitura
+
+fclose (arq1);
+
+
+
+
+
+
--- main.c
+++ main.c
@@ -13,14 +13,18 @@ int main(int argc, char *argv[])
 
     // Teste LSE
     l = lse_cria_lista();
-    l = lse_insere(l, (Login){1, "teste"});
+    l = lse_insere(l, (Login){1, "test"});
     if(strcmp(lse_consulta(l, 1), "teste") == 0)
         puts("Teste LSE: Senha correta!");
+    else
+        printf("Senha incorreta");
 
     // Teste RN
-    t = rn_insere(t, (Login){1, "teste"});
+    t = rn_insere(t, (Login){1, "test"});
     if(strcmp(rn_consulta(t, 1), "teste") == 0)
         puts("Teste RN: Senha correta!");
+    else
+        printf("Senha incorreta");
 
     return 0;
 }
